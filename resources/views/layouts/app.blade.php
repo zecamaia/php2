ec87fd70ea2aa79af2631c77f1de22d79bfc47fc
JOSE_HENRIQUE_MAIA_NETO <12202517@ALFAB02.BARROCA>
2024-06-25 11:46:29 -0300

    Primeiro commit: Adicionando arquivos iniciais

---
+++ resources/views/layouts/app.blade.php
@@ -0,0 +1,47 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="style.css">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Roboto:wght@100;300;400;500&display=swap" rel="stylesheet">
+    <title>Document</title>
+</head>
+<body>
+    <div id="flex">
+        <div id="bordaBotao">
+            <div id="botao">CRIAR TABELA</div> 
+        </div>
+
+        <div id="bordaBotao">
+            <div id="botao">Excluir</div> 
+        </div>
+
+        <div id="bordaBotao">
+            <div id="botao">Editar</div> 
+        </div>
+    </div>
+
+
+
+    
+    
+
+
+    <div id="fundo">
+
+        <div id="barraTopo">
+
+        <table id="tabelaDois">
+            <tr>
+                <td class="td2">Usuario da Força</td>
+                <td class="td2">Espécie</td>
+                <td class="td2">Cor Sabre De Luz</td>
+            </tr>
+        </table>
+        </div>
+    </div>
+</body>
+</html>
